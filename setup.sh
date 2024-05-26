46760b44e59cb500dce02d07b39d8016e08a6c38
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2024-05-26 17:06:56 +0000

    :rocket:FEATURE: Includes cmake installation on setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ export MY_INSTALL_DIR=$HOME/.local;
 mkdir -p $MY_INSTALL_DIR;
 export PATH="$MY_INSTALL_DIR/bin:$PATH";
 
+wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.29.3/cmake-3.29.3-linux-x86_64.sh;
+sh cmake-linux.sh -- --skip-license --prefix=$MY_INSTALL_DIR;
+rm cmake-linux.sh;
+
+sudo apt install -y build-essential autoconf libtool pkg-config
+
 cd ../;
 
 git clone --recurse-submodules -b v1.62.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc;
